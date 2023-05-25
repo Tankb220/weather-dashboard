9abf8741f5b294953d86913d139992e20d18ed32
James Belcher <130000791+Tankb220@users.noreply.github.com>
2023-05-25 14:57:29 -0500

    created history for seaches and forcast dates

--- assets/js/script.js
+++ assets/js/script.js
@@ -132,4 +132,39 @@ function futureWeatherSearch(cityName) {
         // document.getElementById('one-day').textContent = oneDay;
     })    
 }
+//creates a forecast history for the weather data
+function renderForecast(arr) {
+    for(i =0; i<arr.length; i++) {
+        var givenDate = arr[i].dt_txt.split(" ")[0]
+        // console.log(givenDate)
 
+        // var weatherIcon = document.getElementById('weather-icon');
+        // var dataIcon = data.weather[0].icon
+        // weatherIcon.setAttribute("src", `https://openweathermap.org/img/wn/${dataIcon}.png`)
+
+        var properDate = givenDate.split('-');
+        console.log(properDate)
+        document.getElementById("date-" + i).textContent = properDate[1] + "/" + properDate[2] + "/" + properDate[0]
+        document.getElementById("icon-" + i).innerHTML = `<img src="https://openweathermap.org/img/wn/${arr[i].weather[0].icon}.png"/>`
+        document.getElementById("temp-" + i).textContent = "Temp: " + arr[i].main.temp + " \u00B0F"
+        document.getElementById("wind-" + i).textContent = "Wind: " + arr[i].wind.speed + " MPH"
+        document.getElementById("humid-" + i).textContent = "Humidity:" + arr[i].main.humidity + "%"
+    }
+}
+//allows user to go back and find cities they have already searched 
+function renderHistory() {
+    document.getElementById("history").innerHTML = "";
+
+    for(i=0; i<listOfCities.length; i++) {
+        var newBtn = document.createElement("button")
+        newBtn.textContent = listOfCities[i];
+        newBtn.addEventListener("click", function(e) {
+            weatherSearch(e.target.textContent)
+            futureWeatherSearch(e.target.textContent)
+        })
+        
+        document.getElementById("history").append(newBtn)
+    }
+}
+
+renderHistory()
